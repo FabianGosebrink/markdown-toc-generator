7ec9da565058a61dcd8c7304b270257dc2216adf
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-05-29 09:41:12 +0200

    adding date

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,3 +2,7 @@ function toggleDarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function getDate() {
+    document.getElementById("date").innerHTML = new Date().getUTCFullYear();
+}
